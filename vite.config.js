658a9361a763cd810bf4526665dd6c04689a4118
Dracosblood <135628439+Dracosblood@users.noreply.github.com>
2023-08-29 12:45:57 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'http://dracosblood.github.io/Appclima'
+  base: 'http://Dracosblood.github.io/Appclima'
 })
